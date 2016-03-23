cc2a0a0469e632cacccdace4b5fc8ea87d605656
Onorio Catenacci <ocatenacci@riis.com>
2016-03-23 11:36:20 -0400

    Removing unneeded dependencies on ex_doc and earmark

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Pragmatic.Mixfile do
 
   def project do
     [app: :pragmatic,
-     version: "0.1.2",
+     version: "0.1.3",
      elixir: "~> 1.2",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
@@ -29,10 +29,7 @@ defmodule Pragmatic.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    [
-      {:ex_doc, "~> 0.11.4"},
-      {:earmark, "~> 0.2.1"}
-    ]
+    []
   end
 
   defp description do
