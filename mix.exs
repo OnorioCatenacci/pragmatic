8428fbedded517bc6933291c84c5eb778bf9c007
Onorio Catenacci <catenacci@ieee.org>
2017-08-16 12:52:40 -0400

    Beginning to add support for native calls on Windows via Rust.
    Added a dependency for rustler as well as starting to create the
    infrastructure files.

--- mix.exs
+++ mix.exs
@@ -4,11 +4,13 @@ defmodule Pragmatic.Mixfile do
   def project do
     [app: :pragmatic,
      version: "0.1.8",
-     elixir: "~> 1.2",
+     elixir: "~> 1.5",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      description: description(),
      package: package(),
+     compilers: [:rustler] ++ Mix.compilers(),
+     rustler_crates: rustler_crates(),
      deps: deps()]
   end
 
@@ -19,7 +21,8 @@ defmodule Pragmatic.Mixfile do
   defp deps do
     [
       {:earmark, "~> 0.2.1", only: :dev},
-      {:ex_doc, "~> 0.11.4", only: :dev}
+      {:ex_doc, "~> 0.11.4", only: :dev},
+      {:rustler, "~> 0.10.1"}
     ]
   end
 
@@ -27,6 +30,13 @@ defmodule Pragmatic.Mixfile do
     """
     A small, simple library to deal with the practical issues arising from using Elixir on Windows
     """
+  end
+
+  defp rustler_crates do
+    [Pragmatic.Windows: [
+        path: "native/pragmatic_windows",
+        mode: (if Mix.env == :prod, do: :release, else: :debug),
+      ]]
   end
 
   defp package do
