ffaed08ad61039ff71e39aaf6a359a37967cae76
Onorio Catenacci <catenacci@ieee.org>
2017-08-22 21:27:21 -0400

    I've started adding the proper calls to interrogate the registry for the 8.3 setting for the volume.  It's still work in progress.

--- native/pragmatic/src/lib.rs
+++ native/pragmatic/src/lib.rs
@@ -1,15 +1,19 @@
+#[macro_use] extern crate winreg;
 #[macro_use] extern crate rustler;
 #[macro_use] extern crate rustler_codegen;
 #[macro_use] extern crate lazy_static;
 
 use rustler::{NifEnv, NifTerm, NifResult, NifEncoder};
+use winreg::RegKey;
+use winreg::enums::*;
+
 
 mod atoms {
     rustler_atoms! {
         atom ok;
         //atom error;
-        //atom __true__ = "true";
-        //atom __false__ = "false";
+        atom __true__ = "true";
+        atom __false__ = "false";
     }
 }
 
@@ -20,8 +24,18 @@ rustler_export_nifs! {
 }
 
 fn add<'a>(env: NifEnv<'a>, args: &[NifTerm<'a>]) -> NifResult<NifTerm<'a>> {
+    let hklm = RegKey::predef(HKEY_LOCAL_MACHINE);
+    let file_system_key = hklm.open_subkey_with_flags("SYSTEM\\CurrentControlSet\\Control\\FileSystem", KEY_READ).unwrap();
+    let eight_dot_three_disabled: u32 = file_system_key.get_value("NtfsDisable8dot3NameCreation").unwrap();
+    let eight_dot_three_disabled_bool = eight_dot_three_disabled == 1;
     let num1: i64 = try!(args[0].decode());
     let num2: i64 = try!(args[1].decode());
 
-    Ok((atoms::ok(), num1 + num2).encode(env))
+    if eight_dot_three_disabled_bool{
+      Ok((atoms::ok(), atoms::__true__()).encode(env))
+    }
+    else{
+      Ok((atoms::ok(), atoms::__false__()).encode(env))
+    }
+
 }
